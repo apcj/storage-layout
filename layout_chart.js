86d6ae724281383c4efcf8539aebf0e19dd063be
Alistair Jones <alistair.jones@gmail.com>
2011-08-23 16:09:06 +0200

    Switch to using SVG instead of HTML divs

--- layout_chart.js
+++ layout_chart.js
@@ -4,7 +4,7 @@ draw_layout_chart = function() {
 
 	var chart = d3.select("body")
 		.append("div")
-		.attr("class", "digran");
+		.attr("class", "diagram");
 
 	var recordTypeDiv = chart.selectAll("div.recordType")
 		.data(layout)
@@ -13,9 +13,28 @@ draw_layout_chart = function() {
 	recordTypeDiv.append("h3")
 		.text(function(d) { return d.recordName + " (" + d3.sum(d.fields, a("size")) + " bytes)"; });
 
-	recordTypeDiv.append("div").attr("class", "record").selectAll("span.field")
-		.data(a("fields"))
-		.enter().append("span").attr("class", "field")
-			.attr("style", function(d) { return "width: " + d.size * 50 + "px"; })
-			.text(a("name"));
+	var vis = recordTypeDiv.append("svg:svg")
+		.attr("width", 1200)
+		.attr("height", 40)
+		.attr("class", "record");
+		
+	var width = function(size) { return size * 32; };
+	
+	var stack = function(fields) {
+		var position = 0;
+		for (var i = 0; i < fields.length; i++) {
+			fields[i].position = position;
+			position += fields[i].size;
+		}
+		return fields;
+	};
+	
+	vis.selectAll("rect")
+		.data(curry(stack, a("fields")))
+		.enter().append("svg:rect").attr("class", "field")
+			.attr("height", 20)
+			.attr("width", curry(width, a("size")))
+			.attr("y", 1)
+			.attr("x",curry(width, a("position")))
+			// .text(a("name"));
 }
